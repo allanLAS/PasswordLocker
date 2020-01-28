f86b1c3d49b6b130b030e96940951272a61e20eb
Allan <allanlumumba@gmail.com>
2020-01-28 10:51:29 +0300

    create a function that displays all users

--- run.py
+++ run.py
@@ -12,4 +12,13 @@ def create_user(firstName, lastName, email, password):
 ##saving users
 def save_user(user):
     user.save_user()
-    
+
+#display all users
+def display_allusers():
+    return User.display_allusers()
+
+#generate a random password
+def generate_randompass():
+    return User.generate_randompass()
+
+
