f8eb55f8d7821fc39853cb5cd4764bfed92a55a6
Allan <allanlumumba@gmail.com>
2020-01-28 14:31:02 +0300

    add READme file

---
+++ README.md
@@ -0,0 +1,33 @@
+## Description
+This is an application used to save a user's credentials for various accounts
+
+# Description
+The app runs on the terminal and a user navigates through it by the use of short codes provided.
+The short codes are:
+ca- create an account 
+lg- log in
+cc - create a new credential
+dc - display a list of all credentials
+ex - exit
+ra - create an account using a randomly generated password
+fc - search for credential using application name
+
+## Operating
+The pc must first have python installed.
+Then run (python3.6 run.py) in the terminal.
+The user must first sign into all their accounts using their passwords inorder to store and manage their password.
+Also the user can also see all the signed in accounts.
+##  Technology used
+I used python to create the application.
+## Troubleshooting
+If you encounter any problems please feel free to contact me on the contacts provided in the web application and also below.
+## Support and contact details
+Contact us on : 072721237
+Email us on: allan@gmail.com
+## Contributors
+[AllanLumumba]
+
+
+## MIT License
+[LICENSE](LICENSE)
+__Copyright (c) {2020}  Allan Lumumba.__
--- user_test.py
+++ user_test.py
@@ -6,10 +6,57 @@ class TestUser(unittest.TestCase):
         '''
         method to run before each test case
         '''
-        self.new_user = User("allan","lasss","cliche","allan@gmail.com","safepass") #create user object
+        self.new_user = User("allan","lass","cliche","allan@gmail.com","safepass") #create user object
     def tearDown(self):
         '''
         cleans up after each test case has 
         been executed
         '''
         User.user_list = []
+
+    def test_init(self):
+        '''
+        to test if object is initialised properly
+        '''
+        self.assertEqual(self.new_user.first_name,"allan")
+        self.assertEqual(self.new_user.last_name,"lass")
+        self.assertEqual(self.new_user.user_name,"cliche")
+        self.assertEqual(self.new_user.email,"allan@gmail.com")
+        self.assertEqual(self.new_user.password,"safepass")
+
+    def test_save_user(self):
+        '''
+         to test if user object is saved
+         into user list
+        '''
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list),1)
+    def test_save_multipleuser(self):
+        '''
+         to test if we can save multiple users
+         to the user list
+        '''
+        self.new_user.save_user()
+        test_user = User("testfirst","testlast","testusername","test@user.com","testpassword")
+        test_user.save_user()
+        self.assertEqual(len(User.user_list),2)
+    def test_display_allusers(self):
+        '''
+        test for method that returns 
+        all users saved
+        '''
+        self.assertEqual(User.display_allusers(),User.user_list) 
+    def test_generate_randompass(self):
+        '''
+         test for method that generates a random password
+         and returns a string
+        '''
+
+        self.new_user.save_user()
+        test_user = User("testfirst","testlast","testusername","test@user.com","testpassword")
+        test_user.save_user()
+        randompass_generated = User.generate_randompass()
+        self.assertEqual(str(randompass_generated),str(randompass_generated))
+
+if __name__ == '__main__':
+    unittest.main()
