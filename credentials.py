12ed40f9906e38e8dee8052466ff50aa15747cbf
Allan <allanlumumba@gmail.com>
2020-01-28 12:43:43 +0300

    create a method that takes in appname and returns the credentials

--- credentials.py
+++ credentials.py
@@ -12,4 +12,26 @@ class Credentials:
         '''
         initialises a new instance of a credential
         '''
-        
+        self.app_name = app_name
+        self.account_name = account_name
+        self.account_password = account_password
+
+    def save_newcredential(self):
+        Credentials.credential_list.append(self)
+    def deletecredential(self):
+        '''
+        delete a credential from credential list
+        '''
+        Credentials.credential_list.remove(self)
+    @classmethod
+    def find_credentialbyappname(cls,name):
+        '''
+        Method that takes in appname and returns a credential that matches that appname.
+        Args:
+        appname to search for
+        returns :
+        credentials of the one that mayches the appname.
+        '''
+        for credentials in cls.credential_list:
+            if credentials.app_name == name:
+                return credentials
