70213f1da12495715d334cbeab178889e926c66a
Allan <allanlumumba@gmail.com>
2020-01-28 12:35:04 +0300

    create a function that initialises a new credential instance

--- credentials.py
+++ credentials.py
@@ -6,3 +6,10 @@ class Credentials:
     '''
     This class genrates a new instance of a user
     '''
+
+    credential_list = []
+    def __init__(self,app_name,account_name,account_password):
+        '''
+        initialises a new instance of a credential
+        '''
+        
