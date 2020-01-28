843492bd252aa8cd465f18745867b7e6177ae316
Allan <allanlumumba@gmail.com>
2020-01-28 10:28:44 +0300

    create a function that creates a new user

---
+++ run.py
@@ -0,0 +1,12 @@
+from user import User
+from credentials import Credentials 
+
+
+###user
+
+# creating a user
+def create_user(firstName, lastName, email, password):
+    new_user = User(firstName, lastName, username, email, password)
+    return new_user
+
+##saving users
