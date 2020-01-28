083c758b03d360edc9fe6e829c125ca70dc0cf38
Allan <allanlumumba@gmail.com>
2020-01-28 13:35:03 +0300

    create a function tearDown that cleans up after test case has been executed

--- credentials_test.py
+++ credentials_test.py
@@ -6,4 +6,12 @@ class TestUser(unittest.TestCase):
         '''
         method that will run before each test case
         '''
-        
+
+        self.new_credential = Credentials("facebook", "allanLumush", "mypass")
+    def tearDown(self):
+        '''
+        cleans after each test case has been executed
+        '''
+        Credentials.credential_list = []
+
+    
